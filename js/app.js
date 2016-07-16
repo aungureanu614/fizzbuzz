33e4ebbca8b2a3b1edf0cb0c6f26dc3a800e42f6
aungureanu614 <aungureanu614@gmail.com>
2016-07-16 14:12:04 -0700

    index.html

---
+++ js/app.js
@@ -0,0 +1,20 @@
+function fizzbuzz(){
+	for(i=1; i<=100; i++){
+		if((i%3 === 0) && (i%5 === 0)){
+			$('body').append('FizzBuzz <br>');
+		}
+		else if (i%3 === 0) {
+			$('body').append('Fizz <br>');
+		}
+		else if (i%5 === 0) {
+			$('body').append('Buzz <br>');
+		}
+		else{
+			$('body').append(i + '<br>');
+		};
+	};
+};
+
+$(document).ready(function(){
+	fizzbuzz();
+});
