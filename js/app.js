080893a65b9dbe25f031aa5293928b43d26f1f7e
aungureanu614 <aungureanu614@gmail.com>
2016-07-16 14:22:40 -0700

    js/app.js

--- js/app.js
+++ js/app.js
@@ -1,17 +1,21 @@
 function fizzbuzz(){
 	for(i=1; i<=100; i++){
+		//check if i is divisible by both 3 and 5
 		if((i%3 === 0) && (i%5 === 0)){
 			$('body').append('FizzBuzz <br>');
 		}
+		//check if i is divisible by 3
 		else if (i%3 === 0) {
 			$('body').append('Fizz <br>');
 		}
+		//check if i is divisible by 5
 		else if (i%5 === 0) {
 			$('body').append('Buzz <br>');
 		}
+		//print i
 		else{
 			$('body').append(i + '<br>');
-		};
+		}
 	};
 };
 
